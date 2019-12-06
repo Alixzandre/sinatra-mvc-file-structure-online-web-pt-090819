15cb2becdaed25839cca93cd3afa374889dfd58b
Alexander Bispo <alexbispo173@gmail.com>
2019-12-06 21:40:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,23 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
   
+ 
+
+    attr_accessor :name, :breed, :age
+
+      @@all = []
+
+    def initialize(name, breed, age)
+     @name = name
+     @breed = breed
+     @age = age
+     @@all << self
+   end
+
+    def self.all
+       @@all
+   end
+
   
   
 end
